23ca75cc200e8ab11683dd1fabc8dc9f80e811f9
Your Name <you@example.com>
2024-11-06 20:16:56 +0530

    commit

--- src/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Transform JavaScript files with Babel
-    },
-    transformIgnorePatterns: [
-      '/node_modules/(?!axios)/',  // Add axios to the transform regex so Jest can process it
-    ],
-  };
-  
